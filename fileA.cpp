9887a076cad7dcfb92d3b196b89e90400f9dfdeb
ImSelyyy <imselyyy@gmail.com>
2025-02-06 14:59:28 -0400

    Update fileA.cpp

--- fileA.cpp
+++ fileA.cpp
@@ -1,23 +1,23 @@
-//fileA - simple arithmetic
 #include <iostream>
 using namespace std;
 
 void functionA() {
     // Developer1: Change this message
-    cout << "Function A: Hello from Developer1!" << endl;
+    cout << "Function A: Performing arithmetic operations." << endl;
 
-	int a = 5, b = 10;
+    int a = 5, b = 10;
 
     // Developer2: Add a simple sum calculation
-
-	
-	// Developer3: Add a simple subtract calculation
-	
-	
+    int sum = a + b;
+    
+    // Developer3: Add a simple subtract calculation
+    int subtract = a - b;
+    
     // Developer4: Modify this output to use the sum calculation
-	// Developer5: Modify this output to use the subtract calculation
-
-    cout << "Sum: " << (a + b) << endl;
+    cout << "Sum: " << sum << endl;
+    
+    // Developer5: Modify this output to use the subtract calculation
+    cout << "Subtract: " << subtract << endl;
 }
 
 int main() {
